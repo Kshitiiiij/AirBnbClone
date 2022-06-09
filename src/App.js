52580c623171de50de778cc3c97614528cb810e9
Kshitij <kshitij_987@protonmail.com>
2022-06-09 23:44:06 +0530

    Added card function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Navbar from "./components/navbar";
 import Hero from "./components/hero";
+import Card from "./components/card"
 
 export default function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Card />
+      
     </>
   );
 }
