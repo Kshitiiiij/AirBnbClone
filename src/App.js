3e1a2e185933a12eab6f4b9af4238baecc121c68
Kshitij <kshitij_987@protonmail.com>
2022-06-11 22:42:02 +0530

    used props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React from "react";
 import Navbar from "./components/navbar";
 import Hero from "./components/hero";
-import Card from "./components/card"
+import Card from "./components/card";
+
+import person from "./images/kangna.webp";
 
 export default function App() {
   return (
     <>
       <Navbar />
       <Hero />
-      <Card />
+      <Card
+        img={person}
+        rating="5.0"
+        reviewCoundt={6}
+        country="India"
+        title="Crying out alound with Kangna"
+        price={136}
+      />
       
+      <Card
+        img={person}
+        rating="5.0"
+        reviewCoundt={6}
+        country="India"
+        title="Crying out alound with Kangna"
+        price={136}
+      />
     </>
   );
 }
--- src/components/card.js
+++ src/components/card.js
@@ -1,19 +1,23 @@
 import React from "react";
-import person from "../images/kangna.webp";
-import star from "../images/star.png"
+import star from "../images/star.png";
 
-export default function card(){
-    return(
-        <div className="card">
-            <img src={person} alt="eprson" className="card--image"/>
-            <div className="card--stats">
-                <img src={star} alt="star" className="card--star"/>
-                <span>5.0</span>
-                <span className="grey">(6)</span>
-                <span className="grey">India</span>
-            </div>
-            <p>Let's cry together with Kangna</p>
-            <p>From <b>10</b>/person</p>
-        </div>
-    )
+export default function card(props) {
+  {
+    console.log(props);
+  }
+  return (
+    <div className="card">
+      <img src={props.img} alt="eprson" className="card--image" />
+      <div className="card--stats">
+        <img src={star} alt="star" className="card--star" />
+        <span>{props.rating}</span>
+        <span className="grey">(6)</span>
+        <span className="grey">{props.country}</span>
+      </div>
+      <p>{props.title}</p>
+      <p>
+        From <b>{props.price}</b>/person
+      </p>
+    </div>
+  );
 }
